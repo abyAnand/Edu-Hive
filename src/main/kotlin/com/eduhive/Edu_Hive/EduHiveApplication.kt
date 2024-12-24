746e7fd7b866543ea06af46820a30d8b54fab49e
abyAnand <abianand382@gmail.com>
2024-12-25 03:01:36 +0530

    Initial commit

---
+++ src/main/kotlin/com/eduhive/Edu_Hive/EduHiveApplication.kt
@@ -0,0 +1,11 @@
+package com.eduhive.Edu_Hive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EduHiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<EduHiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/eduhive/Edu_Hive/EduHiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eduhive.Edu_Hive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EduHiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
